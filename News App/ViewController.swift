2fafeaf76b32ce395a4d65565aa697ff931b729b
Ahmed Maad <maad@Ahmeds-Mac.local>
2021-06-04 07:24:59 -0700

    first commit

---
+++ News App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News App
+//
+//  Created by Ahmed Maad on 6/4/21.
+//  Copyright © 2021 Next Trend. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
